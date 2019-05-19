1b8cbd79e80aa1bc1094043a07656330add20b02
bellissima0419 <myrealgeorgia@gmail.com>
2019-05-19 03:36:56 -0400

    Organized file tree

--- MR_files/diffs_latlong.py
+++ MR_files/diffs_latlong.py
@@ -36,15 +36,16 @@ def computeNearestZip(latitude, longitude):
                     smallestDistance = distance
 
 
-    return smallestDistance, smallestZip
+    # return smallestDistance, smallestZip
+    return smallestZip
 
-if __name__ == '__main__':
-    #lat long of 30339
-    smallestDistance, smallestZip = computeNearestZip(33.8677, -84.4645)
-    print(smallestZip)
-    print(smallestDistance)
+# if __name__ == '__main__':
+#     #lat long of 30339
+#     smallestDistance, smallestZip = computeNearestZip(33.8677, -84.4645)
+#     print(smallestZip)
+#     print(smallestDistance)
 
-    #lat long of 27510
-    smallestDistance, smallestZip = computeNearestZip(35.9108, -79.0815)
-    print(smallestZip)
-    print(smallestDistance)
+#     #lat long of 27510
+#     smallestDistance, smallestZip = computeNearestZip(35.9108, -79.0815)
+#     print(smallestZip)
+#     print(smallestDistance)
