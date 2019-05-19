e5b98c2d28cfb22c9bcbceeb103102bd732bd978
bellissima0419 <myrealgeorgia@gmail.com>
2019-05-19 03:14:34 -0400

    housekeeping

---
+++ MR_testFiles/diffs_latlong.py
@@ -0,0 +1,51 @@
+from math import sin, cos, sqrt, atan2, radians
+import math
+import json
+fname = 'us-zip-code-latitude-and-longitude.json'
+loadedLatLongData = json.load(open(fname))
+
+
+def distanceV(lat1, lon1,lat2, lon2):
+    #lat1, lon1 = origin
+    #lat2, lon2 = destination
+    radius = 6371 # km
+
+    dlat = math.radians(lat2-lat1)
+    dlon = math.radians(lon2-lon1)
+    a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(math.radians(lat1)) \
+        * math.cos(math.radians(lat2)) * math.sin(dlon/2) * math.sin(dlon/2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    d = radius * c
+
+    return d
+
+def computeNearestZip(latitude, longitude):
+    smallestDistance = 1000000
+    smallestZip = None
+    for data in loadedLatLongData:
+        if ("fields" in data):
+            distancedata = data["fields"]
+            if("latitude" in distancedata and "longitude" in distancedata and "zip" in distancedata):
+                zipcode = distancedata["zip"]
+                lat1 = distancedata["latitude"]
+                long1 = distancedata["longitude"]
+                distance = distanceV(latitude,longitude, lat1,long1)
+
+                if(distance <smallestDistance):
+                    smallestZip = zipcode
+                    smallestDistance = distance
+
+
+    # return smallestDistance, smallestZip
+    return smallestZip
+
+# if __name__ == '__main__':
+#     #lat long of 30339
+#     smallestDistance, smallestZip = computeNearestZip(33.8677, -84.4645)
+#     print(smallestZip)
+#     print(smallestDistance)
+
+#     #lat long of 27510
+#     smallestDistance, smallestZip = computeNearestZip(35.9108, -79.0815)
+#     print(smallestZip)
+#     print(smallestDistance)
---
+++ README.md
@@ -0,0 +1,39 @@
+# ETL_PROJECT
+
+### Going Hunting for data 🏹  📊
+
+<div>
+  <img src="./images/hunterBoy.GIF">
+<div>
+
+#### Contributors:
+https://github.com/patcasrom
+
+https://github.com/bellissima0419
+
+
+<!-- ETL PROJECT -->
+
+
+## View Our Notebooks here:
+
+<!-- https://github.com/bellissima0419/ETL_PROJECT/blob/master/MR_testFiles/census_yelp_combined.ipynb -->
+
+
+
+
+[SQL ETL Notebook](https://nbviewer.jupyter.org/github/bellissima0419/ETL_PROJECT/blob/master/PC_testFiles/ETL%20Project_PCR_FV.ipynb)
+
+[LA CRIME SOURCE DATA](https://catalog.data.gov/dataset/crime-data-from-2010-to-present)
+
+
+[mongo ETL Notebook 1](https://nbviewer.jupyter.org/github/bellissima0419/ETL_PROJECT/blob/master/MR_testFiles/census_yelp_combined.ipynb)
+
+[mongo ETL Notebook 2](https://nbviewer.jupyter.org/github/bellissima0419/ETL_PROJECT/blob/master/MR_testFiles/LA_yelp.ipynb)
+
+
+
+
+<!-- <div>
+  <img src="./images/hunterGirl.GIF">
+<div> -->
